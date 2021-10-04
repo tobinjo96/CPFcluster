cd4ecb5f7f2d826fe4e5e89b2519c7574fadef25
Joshua Tobin <69868231+tobinjo96@users.noreply.github.com>
2021-10-04 14:25:10 +0100

    Delete data directory

--- data/download_data.sh
+++
@@ -1,4 +0,0 @@
-python3 data/download_data.py --data 'covtype' --filepath 'data/covtype.npy'
-python3 data/download_data.py --data 'kddcup' --filepath 'data/kddcup.npy'
-python3 data/download_data.py --data 'YTB' --filepath 'data/YTB.npy'
-python3 data/download_data.py --data 'MSCM1' --filepath 'data/MSCM1.npy'

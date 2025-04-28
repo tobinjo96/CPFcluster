aba39507911241642e6a4dd7c87254d65e38f0ff
FunClub <103412902+Functional-Data-Clustering@users.noreply.github.com>
2025-04-28 03:36:21 +0100

    Update Code Example Geo.py

--- Spatial-CPF/Code Example Geo.py
+++ Spatial-CPF/Code Example Geo.py
@@ -35,7 +35,7 @@ def main():
     # Fit the model for a range of min_samples values.
     print("Fitting CPFcluster...")
     geo_neighbor_adjacency_matrix = np.load("Data/geo_neighbor_adjacency_matrix.npy")
-    cpf.fit(X, geo_neighbor_adjacency_matrix, k_values=[75])
+    cpf.fit(X, geo_neighbor_adjacency_matrix, k_values=[65, 70, 75])
 
     # Perform cross-validation to find the best (min_samples, rho, alpha, merge_threshold, density_ratio_threshold)
     print("Performing cross-validation...")
